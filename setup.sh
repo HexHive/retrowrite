0a557d6f06d598ee1ddb05282792376eddd1f40f
Sushant Dinesh <sushant.dinesh94@gmail.com>
2018-10-17 04:26:06 -0400

    Fixes requirements and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-PATH=`pwd`
+CUR=`pwd`
 
 python3 -m venv retro
 source retro/bin/activate
 pip install --upgrade pip
-pip install -r reqruirements.txt
+pip install -r requirements.txt
 git submodule update --init --checkout third-party/capstone
 cd third-party/capstone
 make
 cd bindings/python/ && make && make install
-cd $PATH
