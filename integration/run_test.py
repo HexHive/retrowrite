cbfa7dd5c57bef753a318db9ee703ddb8141f380
cyanpencil <lucadb96@gmail.com>
2022-05-01 15:00:27 +0200

    Experiment: SPEC CPU++ 17 COUNTERS

--- integration/run_test.py
+++ integration/run_test.py
@@ -83,6 +83,9 @@ for binary_full in sys.argv[1:]:
     if len(binary.split("_")) < 2: 
         quit(f"{binary} wrong format")
 
+    if binary not in ["lbm_r"]:
+        continue
+
     binary_original_name = "_".join(binary.split("_")[:2])
 
     if binary_original_name not in tests.keys():
