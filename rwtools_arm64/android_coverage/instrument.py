de8d7c2acb68605d620e1d99dcd6c2ae81f10c65
cyanpencil <lucadb96@gmail.com>
2022-11-09 16:39:25 +0100

    add ability to specify library paths

--- rwtools_arm64/android_coverage/instrument.py
+++ rwtools_arm64/android_coverage/instrument.py
@@ -183,6 +183,16 @@ mrs x7, nzcv
 // mrs x7, CPSR
 
 mov x9, x0
+
+stp x0, x9, [sp, #-16]!
+adr x0, mystring
+adr x1, myarea
+ldr x1, [x1]
+ldr x1, [x1]
+bl printf
+ldp x0, x9, [sp], #16
+
+
 //ldr x0, =__afl_setup_failure
 
 //ldr x0, =__afl_area_ptr
