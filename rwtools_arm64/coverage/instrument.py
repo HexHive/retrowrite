4a7193bb3a35aeb77fbb04b8f4324cb5f56b42f3
cyanpencil <lucadb96@gmail.com>
2023-03-08 11:07:43 +0100

    fix broken coverage commit, again

--- rwtools_arm64/coverage/instrument.py
+++ rwtools_arm64/coverage/instrument.py
@@ -62,7 +62,13 @@ class Instrument():
                 fn.cache[2].instrument_before(self.get_mem_instrumentation(fn.cache[2], idx, free_registers))
             for idx, instruction in enumerate(fn.cache):
 
-                if instruction.mnemonic.startswith("b") and idx+1 < len(fn.cache):
+                if (instruction.mnemonic.startswith("b.") or \
+                    instruction.mnemonic.startswith("cb") or \
+                    instruction.mnemonic.startswith("cs") or \
+                    instruction.mnemonic.startswith("cc") or \
+                    instruction.mnemonic.startswith("cin") or \
+                    instruction.mnemonic.startswith("cneg") or \
+                    instruction.mnemonic.startswith("tb")) and idx+1 < len(fn.cache):
                     next_instruction = fn.cache[idx+1] # we need to instrument the instruction after the branch
                     if "invalid" in str(next_instruction): continue
                     free_registers = fn.analysis['free_registers'][idx+1] if fn.analysis else None
