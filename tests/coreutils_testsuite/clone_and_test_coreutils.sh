511ddd01e7b18e9a70dfc00fecea28eb8e6d0ba5
cyanpencil <lucadb96@gmail.com>
2023-04-07 18:35:08 +0200

    updated coreutils testing script

--- tests/coreutils_testsuite/clone_and_test_coreutils.sh
+++ tests/coreutils_testsuite/clone_and_test_coreutils.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# clones coreutils source, compiles, rewrites every single binary, 
+# clones coreutils source, compiles, rewrites every single binary,
 # and runs the test suite "make check"
 
 set -e
+
+apt install autoconf automake bison gettext rsync git autopoint gperf texi2html texinfo -y
 
 function clone_coreutils() {
 	git clone git://git.sv.gnu.org/coreutils
@@ -12,17 +14,24 @@ function clone_coreutils() {
 
 [[ ! -d "coreutils" ]] && clone_coreutils;
 
-apt install autopoint gperf texi2html texinfo
 
 cd coreutils
 ./bootstrap
 
+FORCE_UNSAFE_CONFIGURE=1 ./configure
+
 make -j 7
 
 for f in $(find src -type f -executable); do
+	file $f | grep -q ELF || continue;
 	[[ ! -f "${f}_original" ]] && cp ${f} ${f}_original
-	../../../retrowrite ${f}_original ${f}.s 
-	../../../retrowrite -a ${f}.s ${f}; 
+	~/retrowrite/retrowrite ${f}_original ${f}.s
+
+	[[ "$f" =~ "src/libstdbuf.so" ]] && \
+		echo "// SHARED" >> ${f}.s && \
+		sed -i "s/\.section \.fake\.init_array/\.section \.init_array/" ${f}.s
+
+	~/retrowrite/retrowrite -a ${f}.s ${f};
 done
 
 make check  # run the test suite with the rewritten binaries
