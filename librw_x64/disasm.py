a1c6140a2fa0777b79d8d89e93c3f45962b7f03a
cyanpencil <lucadb96@gmail.com>
2024-05-13 10:56:44 +0200

    fix capstone options mismatch

--- librw_x64/disasm.py
+++ librw_x64/disasm.py
@@ -3,6 +3,6 @@ from capstone import *
 
 def disasm_bytes(bytes, addr):
     md = Cs(CS_ARCH_X86, CS_MODE_64)
-    md.syntax = CS_OPT_SYNTAX_ATT
+    md.syntax = 2 # CS_OPT_SYNTAX_ATT
     md.detail = True
     return list(md.disasm(bytes, addr))
